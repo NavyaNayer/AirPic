8fd657f37784b7cde5d0d39b99dd77442588f0a3
jaya2 <jaya2004kra@gmail.com>
2023-09-02 23:23:48 +0530

    added bottom bar, with shutter button, gallery button  & flip camera button

--- Jayashre/app/src/main/java/com/example/airpic/ui/camera/CameraScreen.kt
+++ Jayashre/app/src/main/java/com/example/airpic/ui/camera/CameraScreen.kt
@@ -1,24 +1,32 @@
 package com.example.airpic.ui.camera
 
 
-import android.graphics.Color
+
 import android.util.Log
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.LinearLayout
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FlipCameraAndroid
+import androidx.compose.material.icons.filled.PhotoLibrary
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -40,50 +48,91 @@ private fun CameraContent() {
 
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
-    val cameraController = remember { LifecycleCameraController(context)}
+    val cameraController = remember { LifecycleCameraController(context) }
 
-    Scaffold (
+    Scaffold(
         modifier = Modifier.fillMaxSize()
 
-    ){ paddingValues: PaddingValues ->
-    AndroidView(
-        modifier = Modifier.fillMaxSize().padding(paddingValues),
-        factory = {context ->
-        PreviewView(context).apply {
-            layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-            setBackgroundColor(Color.BLACK)
-            scaleType = PreviewView.ScaleType.FILL_START
-        }.also {previewView ->
-            previewView.controller = cameraController
-            cameraController.bindToLifecycle(lifecycleOwner)
-        }
-    })
+    ) { paddingValues: PaddingValues ->
+        AndroidView(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            factory = { context ->
+                PreviewView(context).apply {
+                    layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+                    setBackgroundColor(android.graphics.Color.BLACK)
+                    scaleType = PreviewView.ScaleType.FILL_START
+                }.also { previewView ->
+                    previewView.controller = cameraController
+                    cameraController.bindToLifecycle(lifecycleOwner)
+                }
+            })
     }
-    val fabSize = with(LocalDensity.current) {80.dp}
-    val fabBackgroundColor = androidx.compose.ui.graphics.Color.White
-    val fabPadding = 16.dp
+    val translucentBlack = Color(0x80f5f5f5)
 
-    // Wrap the FAB inside a Box to control its position
+    val galleryButtonModifier = Modifier
+        .size(with(LocalDensity.current) { 70.dp })
+        .offset(x = 40.dp, y = 75.dp)
+
+    val shutterButtonModifier = Modifier
+        .size(with(LocalDensity.current) { 100.dp })
+        .offset(x = 155.dp, y = 55.dp)
+
+    val flipCameraButtonModifier = Modifier
+        .size(with(LocalDensity.current) { 70.dp })
+        .offset(x = (300.dp), y = 75.dp)
+
+
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(bottom = fabPadding)
-            .offset (x= -(165.dp) , y=-(75.dp)),
+            .fillMaxSize(),
         contentAlignment = Alignment.BottomEnd
     ) {
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(215.dp)
+                .background(translucentBlack)
+        ){FloatingActionButton(
+            onClick = {
+                Log.d("CameraScreen", "Gallery button clicked")
+            },
+            modifier = galleryButtonModifier,
+            backgroundColor = androidx.compose.ui.graphics.Color(0xFF330066),
+            contentColor = androidx.compose.ui.graphics.Color(0XFFFDFEFF)
+        ) {
+            Icon(
+                imageVector = Icons.Default.PhotoLibrary,
+                contentDescription = null,
+                modifier = Modifier.size(32.dp)
+            )
+        }
         FloatingActionButton(
             onClick = {
-                // Handle button click action here
-                Log.d("CameraScreen", "Shutter button clicked") // Log message to the terminal
+                Log.d("CameraScreen", "Shutter button clicked")
             },
-            modifier = Modifier
-                .size(fabSize),
-            backgroundColor = fabBackgroundColor,
+            modifier = shutterButtonModifier,
+            backgroundColor = androidx.compose.ui.graphics.Color.White,
             contentColor = androidx.compose.ui.graphics.Color.White
         ) {
-            // No content inside the button (empty)
+
+        }
+
+        FloatingActionButton(
+            onClick = {
+                Log.d("CameraScreen", "Flip camera button clicked")
+            },
+            modifier = flipCameraButtonModifier,
+            backgroundColor = androidx.compose.ui.graphics.Color(0xFF330066),
+            contentColor = androidx.compose.ui.graphics.Color(0XFFFDFEFF)
+        ) {
+            Icon(
+                imageVector = Icons.Default.FlipCameraAndroid,
+                contentDescription = null,
+                modifier = Modifier.size(30.dp)
+            )
         }
     }
+}
 }
 
 @Preview
