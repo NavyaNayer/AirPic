15767fb6e28ed4d27fbfc5d86cac5d52b9535a8e
jaya2 <jaya2004kra@gmail.com>
2023-11-05 14:21:32 +0530

    modified camera model

--- Jayashre/app/src/main/java/com/example/airpic/ui/camera/CameraViewModel.kt
+++ Jayashre/app/src/main/java/com/example/airpic/ui/camera/CameraViewModel.kt
@@ -5,28 +5,12 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class CameraViewModel : ViewModel() {
+class CameraViewModel: ViewModel() {
 
-    private val _state = MutableStateFlow(CameraState())
-    val state = _state.asStateFlow()
+    private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+    val bitmaps = _bitmaps.asStateFlow()
 
-    fun onPhotoCaptured(bitmap: Bitmap) {
-        // TODO: Process your photo, for example store it in the MediaStore
-        // here we only do a dummy showcase implementation
-        updateCapturedPhotoState(bitmap)
-    }
-
-    fun onCapturedPhotoConsumed() {
-        updateCapturedPhotoState(null)
-    }
-
-    private fun updateCapturedPhotoState(updatedPhoto: Bitmap?) {
-        _state.value.capturedImage?.recycle()
-        _state.value = _state.value.copy(capturedImage = updatedPhoto)
-    }
-
-    override fun onCleared() {
-        _state.value.capturedImage?.recycle()
-        super.onCleared()
+    fun onTakePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
     }
 }
