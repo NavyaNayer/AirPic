66aab31b59d2e4ad853038d8612c37c52c97ac6b
jaya2 <jaya2004kra@gmail.com>
2023-11-06 20:29:25 +0530

    updated gradle file

--- Jayashre/app/build.gradle.kts
+++ Jayashre/app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.material:material:1.9.0")
+    implementation ("io.coil-kt:coil-compose:1.4.0")
     implementation("com.google.ar.sceneform:filament-android:1.17.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
