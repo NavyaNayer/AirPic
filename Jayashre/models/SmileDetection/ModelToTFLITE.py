f33c5f5e9542ebc0b518c25e191e6dbf3779a23c
jaya2 <jaya2004kra@gmail.com>
2023-11-18 19:41:42 +0530

    modified path

--- Jayashre/models/SmileDetection/ModelToTFLITE.py
+++ Jayashre/models/SmileDetection/ModelToTFLITE.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 # Load the Keras model
 model = tf.keras.models.load_model(
-    "C:\\Users\\jaya2\\AndroidStudioProjects\\airpic\\Jayashre\\models\\SmileDetection\\vgg_model_ft_one.h5"
+    "C:\\Users\\jaya2\\Visual Code\\AirPic\\Jayashre\\models\\SmileDetection\\vgg_model_ft_one.h5"
 )
 
 # Convert the model to TFLite format
@@ -11,7 +11,7 @@ tflite_model = converter.convert()
 
 # Save the TFLite model to a file
 with open(
-    "C:\\Users\\jaya2\\AndroidStudioProjects\\airpic\\Jayashre\\models\\SmileDetection\\smile_detection_model.tflite",
+    "C:\\Users\\jaya2\\Visual Code\\AirPic\\Jayashre\\models\\SmileDetection\\smile_detection_model_95.tflite",
     "wb",
 ) as f:
     f.write(tflite_model)
